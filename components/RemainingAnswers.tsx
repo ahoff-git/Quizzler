01528ee710a9f4fc029d1625bc3cc975d1007f16
ahoff-git <76222790+ahoff-git@users.noreply.github.com>
2025-08-17 12:09:11 -0700

    refactor: consolidate stats into generic component

--- components/RemainingAnswers.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-interface RemainingAnswersProps {
-  remaining: number;
-}
-
-export default function RemainingAnswers({ remaining }: RemainingAnswersProps) {
-  return <div>Remaining: {remaining}</div>;
-}
--- components/TotalGuesses.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-interface TotalGuessesProps {
-  total: number;
-}
-
-export default function TotalGuesses({ total }: TotalGuessesProps) {
-  return <div>Guesses: {total}</div>;
-}
